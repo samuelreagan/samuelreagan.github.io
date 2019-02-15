d43f12370b9c70dc450b2c806b38c32405e6d562
samuelreagan <samreagan96@gmail.com>
2019-02-15 17:51:56 -0600

    added card scroll

--- js/main.js
+++ js/main.js
@@ -1,6 +1,57 @@
 var aboutCard = document.getElementById("about");
-aboutCard.style.position = "absolute";
-aboutCard.style.top = "0";
-aboutCard.style.background = "white";
-aboutCard.style.left = "100vw";
-//aboutCard.style.display = "none";
+var projectCard = document.getElementById("projects");
+var aboutBtn = document.getElementById("abtBtn");
+var projectBtn = document.getElementById("projectBtn");
+
+var body = document.getElementsByTagName("body")[0];
+
+function displayCard(card, callback) {
+  card.style.display = "block";
+  setTimeout(function() {
+    callback();
+    setTimeout(function() {
+      body.style.overflow = "scroll";
+    }, 300);
+  },100);
+}
+
+function hideCard(card, callback) {
+  body.style.overflow = "hidden";
+  callback();
+  setTimeout(function() {
+    card.style.display = "none";
+  },500);
+}
+
+aboutBtn.onclick = function() {
+  displayCard(aboutCard, showAbtCard);
+}
+
+aboutCard.onclick = function() {
+  hideCard(aboutCard, hideAbtCard);
+}
+
+function showAbtCard() {
+  aboutCard.style.right = "0";
+}
+
+function hideAbtCard() {
+  aboutCard.style.right = "100vw";
+}
+
+
+projectBtn.onclick = function() {
+  displayCard(projectCard, showProjectCard);
+}
+
+projectCard.onclick = function() {
+  hideCard(projectCard, hideProjectCard);
+}
+
+function showProjectCard() {
+  projectCard.style.left = "0";
+}
+
+function hideProjectCard() {
+  projectCard.style.left = "100vw";
+}
