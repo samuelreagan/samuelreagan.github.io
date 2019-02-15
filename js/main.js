c7f4323ac83d4d87f594dacf42582f17cbe29a92
samuelreagan <samreagan96@gmail.com>
2019-02-15 16:49:47 -0600

    Js test

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
-alert("hi");
+var aboutCard = document.getElementById("about");
+aboutCard.style.position = "absolute";
+aboutCard.style.top = "0";
+aboutCard.style.background = "white";
+aboutCard.style.left = "100vw";
+//aboutCard.style.display = "none";
